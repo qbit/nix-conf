48505e956726947abddbd4a54c365400d1c2241c
Aaron Bieber <aaron@bolddaemon.com>
2021-06-09 10:56:50 -0600

    remove ++ []

--- dbuild/build-server.nix
+++ dbuild/build-server.nix
@@ -8,7 +8,7 @@ with lib; {
   };
 
   config = mkIf config.buildServer.enable {
-    users.users.root.openssh.authorizedKeys.keys = users.users.root.openssh.authorizedKeys.keys ++ [
+    users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEtoU6ObMP7wmglT7rXMg0HEnh7cGBo6COL7BpmRC/o"
     ];
   };
