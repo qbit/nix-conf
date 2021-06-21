22db8ce30e3ef602ce27642faf0e9c4fb9a34178
Aaron Bieber <aaron@bolddaemon.com>
2021-06-21 14:23:38 -0600

    put keys / knownHosts in consumer

--- dbuild/build-consumer.nix
+++ dbuild/build-consumer.nix
@@ -6,12 +6,25 @@ with lib; {
   };
 
   config = mkIf config.buildConsumer.enable {
+    programs.ssh.knownHosts = {
+      pcake = {
+        hostNames = [ "pcake" "pcake.tapenet.org" "10.6.0.202" ];
+        publicKey =
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHgqVw3QWNG6Ty5o2HwW+25Eh59W3lZ30+wMqTEkUZVH";
+      };
+    };
+    programs.ssh.extraConfig = ''
+      Host pcake
+      	HostName 10.6.0.202
+      	IdentitiesOnly yes
+      	IdentityFile /root/.ssh/nix_remote
+    '';
     nix.buildMachines = [{
       hostName = "pcake";
       systems = [ "x86_64-linux" "aarch54-linux" ];
       maxJobs = 2;
       speedFactor = 4;
-      supportedFeatures = [ "kvm" "big-parallel" ];
+      supportedFeatures = [ "kvm" "big-parallel" "nixos-test" "benchmark" ];
       mandatoryFeatures = [ ];
     }];
 
