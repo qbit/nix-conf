5ddc9db34c75fb38f1b616a80448bed396d64ba7
Aaron Bieber <aaron@bolddaemon.com>
2021-06-21 13:33:22 -0600

    add pinebook key

--- dbuild/build-server.nix
+++ dbuild/build-server.nix
@@ -10,6 +10,7 @@ with lib; {
   config = mkIf config.buildServer.enable {
     users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEtoU6ObMP7wmglT7rXMg0HEnh7cGBo6COL7BpmRC/o"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGyOQdqfm7mG+5rOGIqPHSaZQdG/4L03dVJnuE1lO1fo"
     ];
   };
 }
