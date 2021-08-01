b193610076b709aa901326af97ada395cdf58f6b
Aaron Bieber <aaron@bolddaemon.com>
2021-07-31 20:27:50 -0600

    add aarch64 to emulatedSystems

--- dbuild/build-server.nix
+++ dbuild/build-server.nix
@@ -8,6 +8,8 @@ with lib; {
   };
 
   config = mkIf config.buildServer.enable {
+    boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
     users.users.root.openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICEtoU6ObMP7wmglT7rXMg0HEnh7cGBo6COL7BpmRC/o"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGyOQdqfm7mG+5rOGIqPHSaZQdG/4L03dVJnuE1lO1fo"
