97f92485ddc71ebd6233b1d4d7f44afe75ea6599
Aaron Bieber <aaron@bolddaemon.com>
2022-02-07 05:49:12 -0700

    use correct options on unstable

--- nix-lockdown.nix
+++ nix-lockdown.nix
@@ -1,11 +1,19 @@
-{ config, lib, ... }:
+{ config, lib, isUnstable, ... }:
 with lib; {
   options = { nixLockdown = { enable = mkEnableOption "Lockdown Nix"; }; };
   config = mkIf config.nixLockdown.enable {
-    nix = {
-      allowedUsers = [ "@wheel" ];
-      trustedUsers = [ "root" "qbit" ];
-      useSandbox = true;
-    };
+    nix = if isUnstable then
+      {
+        settings.sandbox = true;
+        settings.trusted-users = [ "@wheel" ];
+        settings.allowed-users = [ "root" "qbit" ];
+      }
+    else 
+      {
+        allowedUsers = [ "@wheel" ];
+        trustedUsers = [ "root" "qbit" ];
+        useSandbox = true;
+      };
+
   };
 }
