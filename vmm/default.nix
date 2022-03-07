a9204f2bda9ede1dab61dab39c00d8420bf13668
Aaron Bieber <aaron@bolddaemon.com>
2022-03-07 04:57:37 -0700

    vmm stuff is all in-nixos now \o/

--- vmm/default.nix
+++
@@ -1,8 +0,0 @@
-{ config, lib, ... }:
-let
-  vmmClock = config.boot.kernelPackages.callPackage ./vmm-clock.nix { };
-  virtioVmmci = config.boot.kernelPackages.callPackage ./virtio-vmmci.nix { };
-in {
-  boot.extraModulePackages = [ virtioVmmci vmmClock ];
-  boot.kernelModules = [ "virtio_vmmci" "vmm_clock" ];
-}
