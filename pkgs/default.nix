60307f0d4aa18d11678b8762fc1b3a65fec22d94
Aaron Bieber <aaron@bolddaemon.com>
2022-03-25 19:30:38 -0600

    +sqlc

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, isUnstable, ... }:
 
 with pkgs; {
-  environment.systemPackages = with pkgs;
-    [ (callPackage ./gitmux.nix { isUnstable = isUnstable; }) ];
+  environment.systemPackages = with pkgs; [
+    (callPackage ./gitmux.nix { isUnstable = isUnstable; })
+    (callPackage ./sqlc.nix { isUnstable = isUnstable; })
+  ];
 }
---
+++ pkgs/sqlc.nix
@@ -0,0 +1,35 @@
+{ lib, buildGoModule, fetchFromGitHub, isUnstable, ... }:
+
+let
+  vendorHash = if isUnstable then
+    "sha256-yPAWW9BZgq/e2eyo68WuBkG5L46A2ZacNmF+bkfyBjo="
+  else
+    "";
+
+in with lib;
+buildGoModule rec {
+  pname = "sqlc";
+  version = "1.12.0";
+
+  src = fetchFromGitHub {
+    owner = "kyleconroy";
+    repo = pname;
+    rev = "v${version}";
+    sha256 = "sha256-YlOkjqkhN+4hL1+KJ0TuqcbQXJad/bHZclgpgFPr4to=";
+  };
+
+  vendorSha256 = vendorHash;
+
+  ldflags = [ "-X main.version=${version}" ];
+
+  proxyVendor = true;
+
+  doCheck = false;
+
+  meta = {
+    description = "Generate type-safe code from SQL";
+    homepage = "https://github.com/kyleconroy/sqlc";
+    license = licenses.mit;
+    maintainers = with maintainers; [ qbit ];
+  };
+}
