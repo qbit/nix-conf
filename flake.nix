55f832f825ff01833a7c250a09271fe8965bd6eb
Aaron Bieber <aaron@bolddaemon.com>
2022-04-30 05:25:27 -0600

    rename a bit

--- flake.nix
+++ flake.nix
@@ -3,32 +3,32 @@
 
   inputs = {
     nixpkgs = { url = "github:NixOS/nixpkgs/nixos-unstable"; };
-    stable = { url = "github:NixOS/nixpkgs/nixos-21.11"; };
+    #stablePkgs = { url = "github:NixOS/nixpkgs/nixos-21.11"; };
   };
 
   outputs = { self, nixpkgs }:
     let
       pkgs = (import nixpkgs) { system = "x86_64-linux"; };
 
-      hosts = map (pkgs.lib.removeSuffix ".nix") (pkgs.lib.attrNames
+      targets = map (pkgs.lib.removeSuffix ".nix") (pkgs.lib.attrNames
         (pkgs.lib.filterAttrs (_: entryType: entryType == "regular")
-          (builtins.readDir ./hosts)));
+          (builtins.readDir ./targets)));
 
-      build-host = host: {
-        name = host;
+      build-target = target: {
+        name = target;
 
         value = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
 
           modules = [
-            (import (./hosts + "/${host}.nix"))
-            (import (./hosts + "/${host}/hardware-configuration.nix"))
+            (import (./targets + "/${target}.nix"))
+            (import (./targets + "/${target}/hardware-configuration.nix"))
           ];
         };
       };
 
     in {
       nixosConfigurations = builtins.listToAttrs
-        (pkgs.lib.flatten (map (host: [ (build-host host) ]) hosts));
+        (pkgs.lib.flatten (map (target: [ (build-target target) ]) targets));
     };
 }
